761049efe58c8030a02bdf1ed19d3e157def7ab1
Sangam Giri <girisangam00@gmail.com>
2024-02-09 19:30:37 +0545

    fix:dart_modules added

--- src/main.dart
+++ src/main.dart
@@ -1,7 +1,12 @@
+import 'package:dart_modules/common.dart';
 import 'app.service.dart' as One;
 import 'package:dio/dio.dart' as dio;
 
 void main() {
   dio.Headers();
   One.test();
+  final env = loadEnv();
+  print(env['DATABASE_URL']);
+  print(env['API_KEY']);
+  print(env['DEBUG']);
 }
